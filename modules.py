02fea4005cfdf3bb4544811e773e3c8e9cc52769
boomkin <szerk.animeblog@gmail.com>
2020-07-30 15:33:12 +0200

    Cleaning up useless things from modules.py

--- modules.py
+++ modules.py
@@ -2,19 +2,12 @@
 import torch.nn as nn
 import torch
 import math
-from torch.nn.functional import glu
-# TODO: Finding out in_filters
 
-# nn.InstanceNorm2D eps=1e-6
-
-
-
-
-class ConvLayer(nn.Module):
+class _ConvLayer(nn.Module):
 
     #This acts as a base Conv Class and the forward func specifies whether 1D or 2D
     def __init__(self,in_filter,out_filter,kernel, stride):
-        super(ConvLayer, self).__init__()
+        super(_ConvLayer, self).__init__()
 
         self.in_filter = in_filter
         self.out_filter = out_filter
@@ -52,14 +45,14 @@ class ConvLayer(nn.Module):
         return out
 
 
-class ConvLayer1D(ConvLayer):
+class ConvLayer1D(_ConvLayer):
 
     def __init__(self, in_filter, out_filter, kernel, stride):
         super().__init__(in_filter,out_filter,kernel, stride)
         self.conv = nn.Conv1d(self.in_filter, self.out_filter, self.kernel, stride, padding=self.padding)
 
 
-class ConvLayer2D(ConvLayer):
+class ConvLayer2D(_ConvLayer):
 
     def __init__(self, in_filter, out_filter, kernel, stride):
         super().__init__(in_filter,out_filter,kernel, stride)
@@ -140,27 +133,6 @@ class PixelShuffle1D(torch.nn.Module):
 
         return x
 
-
-
-
-
-class PixelShuffler(nn.Module):
-
-    def __init__(self,shuffle_size=2):
-        super().__init__()
-        self.shuffle_size = shuffle_size
-
-    def forward(self, x):
-        n,c,w = x.size()
-
-        ow = w * self.shuffle_size
-        oc = c // self.shuffle_size
-        #ow = c // self.shuffle_size
-        #oc = w * self.shuffle_size
-
-        return torch.reshape(x, shape = [n,oc,ow])
-
-
 class Upsample1D(nn.Module):
 
     def __init__(self,in_filter,kernel,stride=1):
@@ -178,105 +150,6 @@ class Upsample1D(nn.Module):
 
     def forward(self, x):
         return self.block(x)
-
-
-class ResnetBlock(nn.Module):
-    """Define a Resnet block"""
-
-    def __init__(self, dim, norm_layer):
-        """Initialize the Resnet block
-        A resnet block is a conv block with skip connections
-        We construct a conv block with build_conv_block function,
-        and implement skip connections in <forward> function.
-        Original Resnet paper: https://arxiv.org/pdf/1512.03385.pdf
-        """
-        super(ResnetBlock, self).__init__()
-        self.conv_block = self.build_conv_block(dim, norm_layer)
-
-    def build_conv_block(self, dim, norm_layer):
-        """Construct a convolutional block.
-        Parameters:
-            dim (int)           -- the number of channels in the conv layer.
-            padding_type (str)  -- the name of padding layer: reflect | replicate | zero
-            norm_layer          -- normalization layer
-            use_dropout (bool)  -- if use dropout layers.
-            use_bias (bool)     -- if the conv layer uses bias or not
-        Returns a conv block (with a conv layer, a normalization layer, and a non-linearity layer (ReLU))
-        """
-        conv_block = []
-
-        p = 1
-
-        conv_block += [nn.Conv1d(dim, dim*2, kernel_size=3, padding=p), norm_layer(dim*2), nn.GLU(dim=1)]
-        conv_block += [nn.Conv1d(dim, dim, kernel_size=3, padding=p), norm_layer(dim)]
-
-        return nn.Sequential(*conv_block)
-
-    def forward(self, x):
-        """Forward function (with skip connections)"""
-        out = x + self.conv_block(x)  # add skip connections
-        return out
-
-
-class ResnetGenerator(nn.Module):
-    """Resnet-based generator that consists of Resnet blocks between a few downsampling/upsampling operations.
-    We adapt Torch code and idea from Justin Johnson's neural style transfer project(https://github.com/jcjohnson/fast-neural-style)
-    """
-
-    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm1d, use_dropout=False, n_blocks=6, padding_type='reflect'):
-        """Construct a Resnet-based generator
-        Parameters:
-            input_nc (int)      -- the number of channels in input images
-            output_nc (int)     -- the number of channels in output images
-            ngf (int)           -- the number of filters in the last conv layer
-            norm_layer          -- normalization layer
-            use_dropout (bool)  -- if use dropout layers
-            n_blocks (int)      -- the number of ResNet blocks
-            padding_type (str)  -- the name of padding layer in conv layers: reflect | replicate | zero
-        """
-        assert(n_blocks >= 0)
-        super(ResnetGenerator, self).__init__()
-
-        model = [nn.ReflectionPad1d(3),
-                 nn.Conv1d(input_nc, ngf, kernel_size=7, padding=0),
-                 nn.InstanceNorm1d(ngf),
-                 nn.ReLU(True)]
-
-        n_downsampling = 2
-        for i in range(n_downsampling):  # add downsampling layers
-            mult = 2 ** i
-            #model += [nn.Conv1d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1),
-            #          nn.InstanceNorm1d(ngf * mult * 2),
-            #          nn.ReLU(True)]
-
-            # Our model uses larger kernels for no apparent reasons
-            model += [Downsample1D(in_filter=ngf * mult, out_filter=ngf*mult*2,kernel=5,stride=2)]
-        mult = 2 ** n_downsampling
-        for i in range(n_blocks):       # add ResNet blocks
-
-            model += [ResnetBlock(ngf * mult, norm_layer=nn.InstanceNorm1d)]
-
-        for i in range(n_downsampling):  # add upsampling layers
-            mult = 2 ** (n_downsampling - i)
-            #model += [nn.ConvTranspose1d(ngf * mult, int(ngf * mult / 2),
-            #                             kernel_size=3, stride=2,
-            #                             padding=1, output_padding=1),
-            #          nn.InstanceNorm1d(int(ngf * mult / 2)),
-            #          nn.ReLU(True)]
-            model += [Upsample1D(in_filter=ngf * mult,kernel=5)]
-
-        model += [nn.ReflectionPad1d(3)]
-        model += [nn.Conv1d(ngf, output_nc, kernel_size=7, padding=0)]
-
-        self.model = nn.Sequential(*model)
-
-    def forward(self, input):
-        """Standard forward"""
-        return self.model(input)
-
-
-
-
 
 
 class Generator(nn.Module):
@@ -305,18 +178,6 @@ class Generator(nn.Module):
 
     def forward(self, x):
         return self.block(x)
-
-
-class Debugger(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-
-    def forward(self, x):
-
-        print("debugger",x.shape)
-        return x
-
 
 class PermuteBlock(nn.Module):
     def __init__(self):
@@ -356,22 +217,4 @@ class Discriminator(nn.Module):
 
 if __name__ == '__main__':
 
-    #print(ResidualBlock1D(1024)(torch.ones((10,1024,20))))
-
-    #print(Downsample1D(10,10,3,1)(torch.ones((10,10,10))))
-
-    # Length should be divisible by 4
-    #print(Generator(24)(torch.ones(1,24,128)).shape)
-
-    gen = ResnetGenerator(24, 24, ngf = 64, norm_layer = nn.BatchNorm1d, use_dropout = False, n_blocks = 6, padding_type = 'reflect')
-    print(gen(torch.ones(1, 24, 128)).shape)
-    #print(padding_utility(6,3,1,6)) # 7,
-    #print(padding_utility(16,1,2,3))
-    #print(Discriminator(1)(torch.ones(1,1,24,128)).shape)
-
-    # 10 x 512 x 6 x 16
-
-    # gt: 10 x 6 x 8 x 1024
-    #print(PixelShuffler()(PixelShuffler()(torch.ones(10,8,8))).shape)
-    #print(Upsample1D(16,16,3)(torch.ones(10,16,4)).shape)
-    #print(ConvLayer1D(10,40,3)(torch.ones(size=(10,10,10))))
+    print("")
